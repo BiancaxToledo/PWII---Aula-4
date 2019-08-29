e972eb2b3c8834e0e7af1f0c7d174166be1d8285
BiancaxToledo <bianca.dharas2@gmail.com>
2019-08-28 22:18:17 -0300

    Exemplo de Módulos

---
+++ exercicio2.ts
@@ -0,0 +1,6 @@
+import {Viagem} from "./Viagem"
+{
+    let vg1 = new Viagem
+    console.log(vg1.distancia())
+    console.log(vg1.consumo())    
+}
--- viagem.ts
+++ viagem.ts
@@ -1,4 +1,4 @@
-class Viagem
+export class Viagem
 
 {
 public TempoGasto:number
